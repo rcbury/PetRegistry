dac3c1fe270956bc4e26b61532cbd589754bc67d
windows\asdf <govno@mail.ru>
2023-01-03 05:40:29 +0500

    sdf

---
+++ Controllers/AnimalCardController.cs
@@ -0,0 +1,22 @@
+﻿using PIS_PetRegistry.DTO;
+using PIS_PetRegistry.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_PetRegistry.Controllers
+{
+    internal class AnimalCardController
+    {
+        public AnimalCardDTO AddAnimalCard(AnimalCardDTO animalCardDTO)
+        {
+            var animalCardModel = new AnimalCard()
+            {
+                ChipId = animalCardDTO.ChipId,
+                Name = animalCardDTO.Name,
+            };
+        }
+    }
+}
---
+++ DTO/AnimalCardDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_PetRegistry.DTO
+{
+    public class AnimalCardDTO
+    {
+        public int Id { get; set; } = 0;
+        public bool IsBoy { get; set; }
+        public string Name { get; set; } = null!;
+        public string Photo { get; set; } = null!;
+        public int FkCategory { get; set; }
+        public int FkShelter { get; set; }
+        public string ChipId { get; set; } = null!;
+    }
+}
