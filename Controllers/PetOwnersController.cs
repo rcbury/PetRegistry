9bfaf79daacf7d0b6b0f07b99f546a98082c1630
DESKTOP-D20BSTN\Roman <romanunylov666@gmail.com>
2023-01-04 16:41:47 +0500

    Создал контроллер для формы с владельцами

---
+++ Controllers/PetOwnersController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIS_PetRegistry.DTO;
+using PIS_PetRegistry.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PIS_PetRegistry.Controllers
+{
+    internal class PetOwnersController
+    {
+        public static List<PhysicalPersonDTO> GetPhysicalPeople()
+        {
+            var physicalPeople = new List<PhysicalPersonDTO>();
+            using (var context = new RegistryPetsContext()) 
+            {
+                foreach (var personInfo in context.PhysicalPeople.ToList()) 
+                {
+                    physicalPeople.Add(new PhysicalPersonDTO()
+                    {
+                        Id = personInfo.Id,
+                        Name = personInfo.Name,
+                        Phone = personInfo.Phone,
+                        Address = personInfo.Address,
+                        Email = personInfo.Email,
+                        FkLocality = personInfo.FkLocality,
+                        FkCountry = personInfo.FkCountry
+                    });
+                }
+            }
+            return physicalPeople;
+        }
+
+        public static List<LegalPersonDTO> GetLegalPeople()
+        {
+            var legalPeople = new List<LegalPersonDTO>();
+            using (var context = new RegistryPetsContext())
+            {
+                foreach (var personInfo in context.LegalPeople.ToList())
+                {
+                    legalPeople.Add(new LegalPersonDTO()
+                    {
+                        Id = personInfo.Id,
+                        Inn = personInfo.Inn,
+                        Kpp = personInfo.Kpp,
+                        Name = personInfo.Name,
+                        Phone = personInfo.Phone,
+                        Address = personInfo.Address,
+                        Email = personInfo.Email,
+                        FkLocality = personInfo.FkLocality,
+                        FkCountry = personInfo.FkCountry
+                    });
+                }
+            }
+            return legalPeople;
+        }
+
+        public static List<LocationDTO> GetLocations()
+        {
+            var locations = new List<LocationDTO>();
+            using (var context = new RegistryPetsContext()) 
+            {
+                foreach (var location in context.Locations.ToList()) 
+                {
+                    locations.Add(new LocationDTO()
+                    {
+                        Id = location.Id,
+                        Name = location.Name
+                    });
+                }
+            }
+            return locations;
+        }
+
+        public static List<CountryDTO> GetCountries() 
+        {
+            var countries = new List<CountryDTO>();
+            using (var context = new RegistryPetsContext())
+            {
+                foreach (var country in context.Countries.ToList())
+                {
+                    countries.Add(new CountryDTO()
+                    {
+                        Id = country.Id,
+                        Name = country.Name
+                    });
+                }
+            }
+            return countries;
+        }
+    }
+}
---
+++ DTO/PhysicalPersonDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_PetRegistry.DTO
+{
+    internal class PhysicalPersonDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string Phone { get; set; } 
+
+        public string Address { get; set; }
+
+        public string? Email { get; set; }
+
+        public int FkLocality { get; set; }
+
+        public int FkCountry { get; set; }
+    }
+}
