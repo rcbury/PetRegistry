f75c8638e5090d4c87d6aed67d4f1ed8d52f04c1
Kirill Ponomaryov <kir.pono@mail.ru>
2023-01-12 18:27:40 +0500

    Added Contracts, change methods for physical and legal person

---
+++ Backend/Models/Contract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_PetRegistry.Backend.Models
+{
+    internal class Contract
+    {
+    }
+}
