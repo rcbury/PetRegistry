9f586322fb2cf8faed29698d704c940c4ade2d31
DESKTOP-0J0G6T3\Roman Mikhailovskikh <romanunylov666@gmail.com>
2023-01-12 12:41:45 +0500

    Added users

---
+++ Backend/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PIS_PetRegistry.Backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public virtual Location? Location { get; set; }
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual Shelter? Shelter { get; set; }
+}
